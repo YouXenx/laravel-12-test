b88cee6e21dcac9817077a708e5c6de67337df54
Xensx <dimas.wanty@gmail.com>
2026-03-29 23:11:45 +0700

    feat(profile): update ProfileRepository with create & update functions

--- app/Repositories/ProfileRepository.php
+++ app/Repositories/ProfileRepository.php
@@ -49,4 +49,44 @@ class ProfileRepository implements ProfileRepositoryInterface
             throw $th; // biar error keliatan
         }
     }
+
+    public function udpate(array $data)
+    {
+        DB::beginTransaction();
+
+        try {
+            $profile = Profile::first();
+
+            if (isset($data['thumbanil'])) {
+                $profile->thumbnail = $data['thumbnail']->store('assets/profiles', 'public');
+            }
+
+            $profile->name = $data['name'];
+            $profile->about = $data['about'];
+            $profile->headman = $data['headman'];
+            $profile->people = $data['people'];
+            $profile->agricultural = $data['agricultural_area'];
+            $profile->total_area = $data['total_area'];
+
+            // WAJIB save dulu sebelum relasi
+            $profile->save();
+
+            // Handle multiple images
+            if (!empty($data['images'])) {
+                foreach ($data['images'] as $image) {
+                    $profile->profileImages()->create([
+                        'image' => $image->store('assets/profiles', 'public'),
+                    ]);
+                }
+            }
+
+            DB::commit();
+
+            return $profile;
+
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th; // biar error keliatan
+        }
+    }
 }
